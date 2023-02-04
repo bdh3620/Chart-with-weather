af7128bad82f135c022a9fefc9626899ba80f46c
백대홍 <bdh3620@naver.com>
2023-02-04 23:44:08 +0900

    first commit

---
+++ How's The Weather?/How's The Weather?/How_s_The_Weather_App.swift
@@ -0,0 +1,17 @@
+//
+//  How_s_The_Weather_App.swift
+//  How's The Weather?
+//
+//  Created by 백대홍 on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct How_s_The_Weather_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
